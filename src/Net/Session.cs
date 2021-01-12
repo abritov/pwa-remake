62e4c3a6b112cb594b773e9c6ffde9e90e3d5454
envy124 <pkgsail.dev@gmail.com>
2021-01-12 18:14:43 +0300

    fix session events -> hot

--- src/Net/Session.cs
+++ src/Net/Session.cs
@@ -8,6 +8,7 @@ using Akka.Event;
 
 class Session : ReceiveActor {
     private readonly ILoggingAdapter log = Context.GetLogger();
+    private long? hostId;
 
     private IProtocol protocol;
     public Session(IProtocol protocol, IActorRef manager)
@@ -21,9 +22,29 @@ class Session : ReceiveActor {
     }
 
     private async void Start() {
-        var events = await protocol.EnterWorld("51.255.67.56:28082", new Account() { Login = "skidrow2", Password = "123456", DefaultRoleIndex = 0 });
-        events.Where(x => x is PrivateChat).Do(x => {
-            Console.WriteLine(((PrivateChat)x).msg);
-        }).Subscribe();
+        var connection = await protocol.EnterWorld(
+            "51.255.67.56:28082", 
+            new Account() { Login = "skidrow2", Password = "123456", DefaultRoleIndex = 0 }
+        );
+        var events = Observable.Publish(connection);
+
+        var awaitConnectionToTheHost = events
+            .Where(x => x is PrivateChat)
+            .Cast<PrivateChat>()
+            .Where(x => x.msg == "hi")
+            .Subscribe(x => {
+                hostId = x.src_id;
+                Console.WriteLine("Connected");
+            });
+
+        var readHostMoves = events
+            .Where(x => x is RpcContainer)
+            .SelectMany(x => ((RpcContainer)x).Packets)
+            .Where(x => x is ObjectMove)
+            .Cast<ObjectMove>()
+            .Where(x => hostId != null && x.id == hostId)
+            .Subscribe(target => Console.WriteLine($"{target.dest.X} {target.dest.Y} {target.dest.Z}"));
+
+        events.Connect();
     }
 }
