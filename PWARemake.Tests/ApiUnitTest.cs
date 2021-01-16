e0fffd928fb9ef2928617c3e0b6eea14196044f0
envy124 <pkgsail.dev@gmail.com>
2021-01-16 20:15:03 +0300

    add tests project

---
+++ PWARemake.Tests/ApiUnitTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace PWARemake.Tests
+{
+    public class ApiTest
+    {
+        [Fact]
+        public void TestSerializeRpcOk()
+        {
+            Console.WriteLine("123");
+        }
+    }
+}
