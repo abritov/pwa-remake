518ab2b26d026d5f5f6cc1789efb2a3f5a969210
envy124 <pkgsail.dev@gmail.com>
2021-01-17 10:05:15 +0300

    add TestSerializeClientRpcOk

--- PWARemake.Tests/ApiUnitTest.cs
+++ PWARemake.Tests/ApiUnitTest.cs
@@ -1,14 +1,19 @@
 using System;
 using Xunit;
+using PWARemake.Protocol;
 
 namespace PWARemake.Tests
 {
     public class ApiTest
     {
         [Fact]
-        public void TestSerializeRpcOk()
+        public void TestSerializeClientRpcOk()
         {
-            Console.WriteLine("123");
+            var api = new PWARemake.Protocol.Api();
+            Assert.Equal(
+                "{\"GameCmd\":{\"cmd\":{\"CmdPlayerMove\":{\"current_pos\":null,\"next_pos\":null,\"use_time\":0,\"speed\":0,\"move_mode\":0,\"stamp\":0}}}}",
+                api.Serialize(new CmdPlayerMove().IntoRpc2())
+            );
         }
     }
 }
